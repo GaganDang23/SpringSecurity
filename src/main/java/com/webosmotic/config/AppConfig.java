9e9b61964e0ec47220d625805b51b5faaf4ad703
GaganDang23 <137985599+GaganDang23@users.noreply.github.com>
2023-06-29 16:33:04 +0530

    Initial commit

---
+++ src/main/java/com/webosmotic/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.webosmotic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//@Configuration
+public class AppConfig {
+//	@Bean
+//	public UserDetailService userDetailService() {
+//
+//		UserDetails userDetails = User.builder().username("gagan").password(passwordEncoder().encode("gagan"))
+//				.roles("ADMIN").build();
+//
+//		return new InMemoryUserDetailsManager(userDetails);
+//	}
+//
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//
+//		return new BCryptPasswordEncoder();
+//	}
+
+}
